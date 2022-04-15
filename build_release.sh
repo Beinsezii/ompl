c54966daec2091f53ab7e8586cbf7967766b5618
Beinsezii <beinsezii@gmail.com>
2022-04-15 12:41:57 -0700

    BUILD strip in Cargo.toml instead of build_release.sh

--- build_release.sh
+++ build_release.sh
@@ -3,4 +3,3 @@ cargo build --release --target x86_64-pc-windows-gnu
 mkdir ./bin 2> /dev/null
 cp ./target/x86_64-unknown-linux-gnu/release/ompl ./bin/
 cp ./target/x86_64-pc-windows-gnu/release/ompl.exe ./bin/
-strip ./bin/*
