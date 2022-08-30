59bb28911cc7140c3310f27c8f16b8d9451995ac
Beinsezii <beinsezii@gmail.com>
2022-08-29 23:35:31 -0700

    BUILD swap script for new pixelbuster script

---
+++ build_bin.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+BIN="ompl"
+FEATURES=""
+
+LINUX="./target/release/${BIN}"
+WIN="./target/release/${BIN}.exe"
+
+cargo build --release $FEATURES
+
+mkdir ./bin 2>/dev/null
+
+if [ -f "$LINUX" ]; then
+    cp "$LINUX" "./bin/${BIN}"
+    cargo build --release $FEATURES --target x86_64-pc-windows-gnu
+    cp "./target/x86_64-pc-windows-gnu/release/${BIN}.exe" "./bin/${BIN}.exe"
+fi
+
+if [ -f "$WIN" ]; then
+    cp "$WIN" "./bin/${BIN}.exe"
+    cargo build --release $FEATURES --target x86_64-unknown-linux-gnu
+    cp "./target/x86_64-unknown-linux-gnu/release/${BIN}" "./bin/${BIN}"
+fi
--- build_release.sh
+++
@@ -1,5 +0,0 @@
-cargo build --release --target x86_64-unknown-linux-gnu
-cargo build --release --target x86_64-pc-windows-gnu
-mkdir ./bin 2> /dev/null
-cp ./target/x86_64-unknown-linux-gnu/release/ompl ./bin/
-cp ./target/x86_64-pc-windows-gnu/release/ompl.exe ./bin/
