f06aebd42b9daa4ea955e5678cfcfc73f0098b83
Beinsezii <beinsezii@gmail.com>
2023-05-17 18:19:24 -0700

    SYMPAL Rest of the fucking owl
    Should be completely up-to-spec with the rodio backend now.
    
    Tested with PipeWire mp3+ogg+flac.
    
    Hypothetically nothing stopping it from working on other audio formats.
    Everything is still upcast to 32-bit right now which
    is memory intensive though...
    
    Plus it also still maxes a thread. I could insert a nano-second sleep
    in there to help but I'm not sure that'd be desireable on anything
    but the most potato of computers. If you're on a RasPi you're probably
    not using this player anyways. Would it even compile on ARM..?
    
    Windows untested as of writing this. Not sure if using pulse-alsa or
    native alsa will be any different from pipewire-alsa either. The only
    issue I can see is no F32 support somewhere.
    
    While it's technically possible to write generic code for handling
    any->any sample format, donig so *sanely* is TBD.
    
    Also bonus points if some format I've never used has planar stereo
    instead of interleaved causing a
    "Help my audio is stuttery/crackly/mono" bug report in the near future.
    
    Additionally, right now I always just play the first track.
    Honestly I'm content to leave it as multi-track music files arent really
    a thing, and IDK who in their mind is playing video clips in a music
    player. Rodio probably does the same thing I'd imagine.
    Who knows, might get proven wrong again...
    
    Uhhh what else..?
    
    I love you.

--- src/library/player/sympal.rs
+++ src/library/player/sympal.rs
@@ -1,42 +1,39 @@
+#![warn(missing_docs)]
+
 use super::{Player, PlayerMessage};
 use crate::library::Track;
 
 use std::{
     fs::File,
     sync::{
-        atomic::{AtomicBool, AtomicU32, Ordering, AtomicUsize},
+        atomic::{AtomicBool, AtomicU32, AtomicUsize, Ordering},
         mpsc::SyncSender,
         Arc, Mutex, RwLock,
     },
     thread,
-    time::{Duration, Instant},
+    time::Duration,
 };
 
 use cpal::{
     traits::{DeviceTrait, HostTrait, StreamTrait},
-    Stream,
+    SampleFormat, SampleRate,
 };
-use symphonia::core::{
-    audio::{AudioBuffer, RawSample, SampleBuffer},
-    conv::ConvertibleSample,
-    io::{MediaSourceStream, ReadBytes},
-    probe::Hint,
-};
+use symphonia::core::{audio::SampleBuffer, io::MediaSourceStream, probe::Hint};
 
 pub struct Backend {
     track: Mutex<Option<Arc<Track>>>,
     volume: Arc<AtomicU32>,
     channel: Option<SyncSender<PlayerMessage>>,
     join: Arc<AtomicBool>,
+    first: Arc<AtomicBool>,
+    last: Arc<AtomicBool>,
+    // TODO: i16 probably better.
+    // Wonder if Windows has as extensive of format support?
+    samples: Mutex<Arc<RwLock<Vec<f32>>>>,
     pos: Arc<AtomicUsize>,
+    rate: Arc<AtomicU32>,
+    channels: Arc<AtomicUsize>,
 }
-
-trait AudioOutputSample:
-    cpal::Sample + ConvertibleSample + RawSample + std::marker::Send + 'static
-{
-}
-
-impl AudioOutputSample for f32 {}
 
 impl Player for Backend {
     fn new(sig: Option<SyncSender<PlayerMessage>>) -> Self
@@ -47,48 +44,33 @@ impl Player for Backend {
             track: Mutex::new(None),
             volume: Arc::new(AtomicU32::from(1.0f32.to_bits())),
             channel: sig,
-            join: Arc::new(AtomicBool::new(false)),
+            join: Arc::new(AtomicBool::new(true)),
+            first: Arc::new(AtomicBool::new(false)),
+            last: Arc::new(AtomicBool::new(false)),
+            samples: Default::default(),
             pos: Arc::new(AtomicUsize::new(0)),
+            rate: Arc::new(AtomicU32::new(0)),
+            channels: Arc::new(AtomicUsize::new(0)),
         }
     }
     fn types(&self) -> Vec<String> {
-        vec![String::from("mp3")]
+        vec![
+            String::from("flac"),
+            String::from("m4a"),
+            String::from("mp3"),
+            String::from("ogg"),
+            String::from("wav"),
+        ]
     }
     fn play(&self) {
+        // {{{
         let channel = if let Some(channel) = &self.channel {
-            channel
+            channel.clone()
         } else {
             return;
         };
 
         self.join.store(true, Ordering::Relaxed);
-
-        let mss = MediaSourceStream::new(
-            Box::new(
-                File::open(self.track.lock().unwrap().as_ref().unwrap().path().clone()).unwrap(),
-            ),
-            Default::default(),
-        );
-
-        let mut fr = symphonia::default::get_probe()
-            .format(
-                Hint::new().with_extension("mp3"),
-                mss,
-                &Default::default(),
-                &Default::default(),
-            )
-            .unwrap()
-            .format;
-
-        let tracks = fr.tracks();
-
-        let mut decoder = symphonia::default::get_codecs()
-            .make(&tracks[0].codec_params, &Default::default())
-            .unwrap();
-
-        let samples = Arc::new(RwLock::new(Vec::new()));
-        let first = Arc::new(AtomicBool::new(false));
-        let (tsamples, tfirst, tj) = (samples.clone(), first.clone(), self.join.clone());
 
         let vol = self.volume.clone();
         let gain = self.track.lock().unwrap().as_ref().unwrap().gain();
@@ -99,30 +81,39 @@ impl Player for Backend {
             .default_output_device()
             .expect("No audio output device found");
 
-        let config = device
-            .default_output_config()
-            .expect("Audio device has no supported configs");
+        let rate = self.rate.load(Ordering::Relaxed);
+        let channels = self.channels.load(Ordering::Relaxed);
+
+        let config = if let Ok(mut configs) = device.supported_output_configs() {
+            loop {
+                match configs.next() {
+                    Some(config) => {
+                        if config.channels() as usize == channels
+                            && config.max_sample_rate().0 >= rate
+                            && config.min_sample_rate().0 <= rate
+                            && config.sample_format() == SampleFormat::F32
+                        {
+                            break config.with_sample_rate(SampleRate(rate));
+                        }
+                    }
+                    None => panic!("No valid config for audio device found!"),
+                }
+            }
+        } else {
+            panic!("No configs for audio device found!")
+        };
 
         self.join.store(false, Ordering::Relaxed);
-
-        thread::spawn( move ||
-            while let Ok(packet) = fr.next_packet() {
-                let ab = decoder.decode(&packet).unwrap();
-                let mut sb = SampleBuffer::<f32>::new(packet.dur, *ab.spec());
-                sb.copy_interleaved_ref(ab);
-                tsamples.write().unwrap().extend_from_slice(sb.samples());
-                tfirst.store(true, Ordering::Relaxed);
-                if tj.load(Ordering::Relaxed) { break }
-            }
-        );
 
         let tj = self.join.clone();
         let uj = self.join.clone();
         let pos = self.pos.clone();
-        pos.store(0, Ordering::Relaxed);
+        let first = self.first.clone();
+        let samples = self.samples.lock().unwrap().clone();
+        let channel_err = channel.clone();
 
         thread::spawn(move || {
-            while ! first.load(Ordering::Relaxed) {
+            while !first.load(Ordering::Relaxed) {
                 thread::sleep(Duration::from_millis(1))
             }
             let stream = device
@@ -130,21 +121,31 @@ impl Player for Backend {
                     &config.config(),
                     move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
                         for sample in data {
-                            let n = pos.load(Ordering::SeqCst);
+                            // Wonder how necessary these locks are.
+                            // IDK much about atomics but this seems to make sense to ensure
+                            // the same pos isn't loaded twice, right?
+                            let n = pos.load(Ordering::Acquire);
                             *sample = gain
                                 * f32::from_bits(vol.load(Ordering::Relaxed)).powi(3)
                                 * match samples.read().unwrap().get(n) {
                                     Some(s) => s,
                                     None => {
-                                        uj.store(true, Ordering::Relaxed);
+                                        if !uj.load(Ordering::Acquire) {
+                                            channel.send(PlayerMessage::Request).unwrap()
+                                        };
+                                        uj.store(true, Ordering::Release);
                                         &0.0
                                     }
                                 };
-                            pos.store(n+1, Ordering::SeqCst);
+                            pos.store(n + 1, Ordering::Release);
                         }
                         // react to stream events and read or write stream data here.
                     },
-                    move |err| {},
+                    move |err| {
+                        channel_err
+                            .send(PlayerMessage::Error(err.to_string()))
+                            .unwrap();
+                    },
                     None, // None=blocking, Some(Duration)=timeout
                 )
                 .expect("Could not initialize audio stream");
@@ -154,17 +155,23 @@ impl Player for Backend {
             }
         });
     }
+    // }}}
 
-    fn stop(&self) {}
-    fn pause(&self) {}
+    fn stop(&self) {
+        self.pause();
+        self.pos.store(0, Ordering::Relaxed);
+    }
+    fn pause(&self) {
+        self.join.store(true, Ordering::Relaxed);
+    }
     fn playing(&self) -> bool {
-        false
+        !self.join.load(Ordering::Relaxed)
     }
     fn paused(&self) -> bool {
-        false
+        self.pos.load(Ordering::Relaxed) != 0 && self.join.load(Ordering::Relaxed)
     }
     fn seekable(&self) -> Option<bool> {
-        Some(false)
+        Some(self.last.load(Ordering::Relaxed))
     }
     fn volume_set(&self, volume: f32) {
         self.volume
@@ -177,8 +184,92 @@ impl Player for Backend {
         self.track.lock().unwrap().clone()
     }
     fn track_set(&self, mut track: Option<Arc<Track>>) -> Option<Arc<Track>> {
+        // {{{
+        self.join.store(true, Ordering::Relaxed);
         let guard: &mut Option<Arc<Track>> = &mut self.track.lock().unwrap();
-        std::mem::swap(guard, &mut track);
+        if *guard == track {
+            self.stop()
+        } else {
+            std::mem::swap(guard, &mut track);
+
+            if let Some(track) = guard.as_ref() {
+                let mss = MediaSourceStream::new(
+                    Box::new(File::open(track.path().clone()).unwrap()),
+                    Default::default(),
+                );
+
+                let mut fr = symphonia::default::get_probe()
+                    .format(
+                        Hint::new()
+                            .with_extension(track.path().extension().unwrap().to_str().unwrap()),
+                        mss,
+                        &Default::default(),
+                        &Default::default(),
+                    )
+                    .expect("SYMPAL Probe result return ERR")
+                    .format;
+
+                let audio_tracks = fr.tracks();
+
+                let mut decoder = symphonia::default::get_codecs()
+                    .make(&audio_tracks[0].codec_params, &Default::default())
+                    .expect("SYMAPL Decoder return ERR");
+
+                let rate = decoder
+                    .codec_params()
+                    .sample_rate
+                    .expect("SYMPAL Decoder has no sample rate");
+                self.rate.store(rate, Ordering::Relaxed);
+                self.channels.store(
+                    decoder
+                        .codec_params()
+                        .channels
+                        .expect("SYMPAL Decoder has no channels")
+                        .count(),
+                    Ordering::Relaxed,
+                );
+
+                *self.samples.lock().unwrap() =
+                    Arc::new(RwLock::new(Vec::with_capacity(rate as usize * 120)));
+
+                let (samples, first, last) = (
+                    Arc::downgrade(&self.samples.lock().unwrap()),
+                    self.first.clone(),
+                    self.last.clone(),
+                );
+
+                // Paranoia, making 100% sure any other decode threads drop first.
+                // Probably not necessary.
+                // Felt cute, might remove later.
+                thread::sleep(Duration::from_millis(1));
+
+                self.first.store(false, Ordering::Relaxed);
+                self.last.store(false, Ordering::Relaxed);
+                self.pos.store(0, Ordering::Relaxed);
+
+                thread::spawn(move || {
+                    while let Ok(packet) = fr.next_packet() {
+                        if packet.dur() < 1 {
+                            // 0 length packets are possible I guess
+                            continue;
+                        }
+                        if let Some(samples) = samples.upgrade() {
+                            let ab = decoder
+                                .decode(&packet)
+                                .expect("SYMPAL Decoder failed to decode packet");
+                            let mut sb = SampleBuffer::<f32>::new(packet.dur, *ab.spec());
+                            sb.copy_interleaved_ref(ab);
+                            samples.write().unwrap().extend_from_slice(sb.samples());
+                            first.store(true, Ordering::Relaxed);
+                        } else {
+                            return;
+                        }
+                    }
+                    last.store(true, Ordering::Relaxed);
+                });
+            }
+        }
         track
     }
+    // }}}
 }
