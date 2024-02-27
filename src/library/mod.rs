7c5c6d519d24cc7dd9144c81c2b19ed28e902dc0
Beinsezii <beinsezii@gmail.com>
2024-02-26 16:42:30 -0800

    LIBRARY Fix track not found logic

--- src/library/mod.rs
+++ src/library/mod.rs
@@ -279,8 +279,9 @@ impl Library {
                     tracks.remove(id);
                 }
                 drop(tracks);
-                self.next();
                 self.force_build_filters();
+                self.next();
+                return;
             }
         }
         if let Some(track) = self.player.play_track(track) {
