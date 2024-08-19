3905b628286a3580e2dbae330aaebb85d1e3397d
Beinsezii <beinsezii@gmail.com>
2024-08-18 22:17:30 -0700

    TUI/Art add manual ANSI color quantization
    Looks much more legible

--- src/tui/widgets/art.rs
+++ src/tui/widgets/art.rs
@@ -1,9 +1,11 @@
 use super::StyleSheet;
 
 use crate::library::Library;
+use crate::logging::*;
 
 use std::sync::Arc;
 
+use ratatui::crossterm::style::available_color_count;
 use ratatui::layout::Rect;
 use ratatui::prelude::Buffer;
 use ratatui::style::{Color, Style};
@@ -25,6 +27,49 @@ fn alpha([r, g, b, a]: [u8; 4]) -> [u8; 3] {
     let a = a as f32 / u8::MAX as f32;
     [r, g, b].map(|c| (c as f32 * a).round() as u8)
 }
+fn pixel2col(rgba: [u8; 4], quantize: bool) -> Color {
+    let [r, g, b] = alpha(rgba);
+    if quantize {
+        let rgb = [r, g, b].map(|c| c.min(254) / (u8::MAX / 3));
+        // prioritize green > red > blue
+        match rgb {
+            [0, 0, 0] => Color::Black,
+            [0, 0, 1] => Color::Blue,
+            [0, 0, 2] => Color::LightBlue,
+            [0, 1, 0] => Color::Green,
+            [0, 1, 1] => Color::Cyan,
+            [0, 1, 2] => Color::Cyan,
+            [0, 2, 0] => Color::LightGreen,
+            [0, 2, 1] => Color::LightCyan,
+            [0, 2, 2] => Color::LightCyan,
+            [1, 0, 0] => Color::Red,
+            [1, 0, 1] => Color::Magenta,
+            [1, 0, 2] => Color::Magenta,
+            [1, 1, 0] => Color::Yellow,
+            [1, 1, 1] => Color::DarkGray,
+            [1, 1, 2] => Color::LightBlue,
+            [1, 2, 0] => Color::LightYellow,
+            [1, 2, 1] => Color::LightGreen,
+            [1, 2, 2] => Color::LightCyan,
+            [2, 0, 0] => Color::LightRed,
+            [2, 0, 1] => Color::LightMagenta,
+            [2, 0, 2] => Color::LightMagenta,
+            [2, 1, 0] => Color::Yellow,
+            [2, 1, 1] => Color::LightRed,
+            [2, 1, 2] => Color::LightMagenta,
+            [2, 2, 0] => Color::LightYellow,
+            [2, 2, 1] => Color::LightYellow,
+            [2, 2, 2] => Color::White,
+
+            _ => {
+                error!("### TUI::Art bad color array {:?} ###", rgb);
+                return Color::Magenta;
+            }
+        }
+    } else {
+        Color::Rgb(r, g, b)
+    }
+}
 
 impl Widget for Art {
     fn render(self, area: Rect, buf: &mut Buffer)
@@ -36,6 +81,7 @@ impl Widget for Art {
         }
         let (w, h) = (area.width as usize, area.height as usize * 2);
         if let Some(thumbnail) = self.library.thumbnail(w, h) {
+            let quantize = available_color_count() <= 16;
             let fill = self.stylesheet.base.bg.unwrap_or(Color::Black);
             // clip at 5% or less
             const CLIP: u8 = u8::MAX / 20;
@@ -60,20 +106,17 @@ impl Widget for Art {
                                 // Uniform solid only draw block to avoid
                                 // terminal emulator transparency
                                 } else if bg == fg {
-                                    let fg = alpha(*fg);
-                                    style = style.fg(Color::Rgb(fg[0], fg[1], fg[2]));
+                                    style = style.fg(pixel2col(*fg, quantize));
                                     "█"
                                 } else {
-                                    let (fg, bg) = (alpha(*fg), alpha(*bg));
-                                    style = style.fg(Color::Rgb(fg[0], fg[1], fg[2])).bg(Color::Rgb(bg[0], bg[1], bg[2]));
+                                    style = style.fg(pixel2col(*fg, quantize)).bg(pixel2col(*bg, quantize));
                                     "▀"
                                 }
                             } else if fg[3] <= CLIP {
                                 style = style.bg(fill);
                                 " "
                             } else {
-                                let fg = alpha(*fg);
-                                style = style.fg(Color::Rgb(fg[0], fg[1], fg[2]));
+                                style = style.fg(pixel2col(*fg, quantize));
                                 "▀"
                             };
 
